53b041d002ae9c43fa7be972c15f985ce6a9152a
philliphue <phue@chicagobooth.edu>
2015-10-22 23:13:59 -0500

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get("",   { :controller => "rps", :action => "rock" })
   get("/rock",   { :controller => "rps", :action => "rock" })
   get("/paper", { :controller => "rps", :action => "paper" })
   get("/scissors", { :controller => "rps", :action => "scissors" })
